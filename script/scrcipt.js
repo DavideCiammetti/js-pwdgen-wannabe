4865eeda4eee0de9add91a586146084465fa8d20
DavideCiammetti <davideciammetti97@gmail.com>
2023-10-31 15:47:14 +0100

    feat: bonus random numb

--- script/scrcipt.js
+++ script/scrcipt.js
@@ -6,6 +6,11 @@ const cognome = document.getElementById('cognome');
 const colorePreferito = document.getElementById('colorePreferito');
 const password = document.getElementById('password');
 
+// genera numeri casuali da 1 a 10
+let min = 1;
+let max = 10;
+let n = Math.floor(Math.random() * (max - min + 1)) + min;
+
 // mostrare nella conosle gli elementi presi
 console.log(nome);
 console.log(cognome);
@@ -17,4 +22,4 @@ nome.innerHTML = nome.innerHTML + prompt('Scrivi il tuo nome');
 cognome.innerHTML= cognome.innerHTML + prompt('Scrivi il tuo cognome');
 colorePreferito.innerHTML = colorePreferito.innerHTML + prompt('Scrivi il tuo colore preferito');
 // mostrare a schermo la password
-password.innerHTML = 'la tua password:' + ' ' + nome.innerHTML + cognome.innerHTML + colorePreferito.innerHTML + '21';
+password.innerHTML = 'la tua password:' + ' ' + nome.innerHTML + cognome.innerHTML + colorePreferito.innerHTML + n;
